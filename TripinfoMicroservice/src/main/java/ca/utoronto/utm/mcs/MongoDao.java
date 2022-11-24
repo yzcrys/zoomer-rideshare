2dff382e3877284baa5b513c154684ba41e1124e
yzcrys <deaddyui@gmail.com>
2022-11-24 17:24:40 -0500

    Create trip/request database operation (#3)

--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
@@ -1,7 +1,24 @@
 package ca.utoronto.utm.mcs;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.CommandResult;
+import com.mongodb.DBObject;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import java.sql.Time;
+import java.util.Arrays;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class MongoDao {
 	
@@ -11,8 +28,35 @@ public class MongoDao {
         // TODO: 
         // Connect to the mongodb database and create the database and collection. 
         // Use Dotenv like in the DAOs of the other microservices.
+
+//		String uri = "mongodb://root:123456@" + System.getenv("MONGODB_ADDR") + ":27017/?maxPoolSize=20&w=majority";
+		String connectionString = "mongodb://root:123456@" + System.getenv("MONGODB_ADDR") + ":27017/root?&ssl=false";
+		System.out.println("the conn string: " + connectionString);
+
+		try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+			MongoDatabase database = mongoClient.getDatabase("trip");
+			collection = database.getCollection("trips");
+
+//			try {
+//				Bson command = new BsonDocument("ping", new BsonInt64(1));
+//				Document commandResult = database.runCommand(command);
+//				System.out.println("Connected successfully to server.");
+//			} catch (MongoException me) {
+//				System.err.println("An error occurred while attempting to run a command: " + me);
+//			}
+		}catch (MongoException me) {
+				System.err.println("An error occurred" + me);
+			}
+
 	}
 
 	// *** implement database operations here *** //
+
+	public void addTripRequest(String uid, Integer radius) {
+		//
+
+		System.out.println("Success! Inserted document");
+	}
+
 
 }
