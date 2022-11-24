490af73ff9d9a5aaa656ac4a6a3430a868931be4
yzcrys <deaddyui@gmail.com>
2022-11-24 17:24:15 -0500

    Create trip/request database operation

--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/App.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -10,6 +10,7 @@ public class App {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
 
         // TODO: Add server contexts here. Do not set executors for the server, you shouldn't need them.
+        server.createContext("/trip/request", new Request());
 
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
