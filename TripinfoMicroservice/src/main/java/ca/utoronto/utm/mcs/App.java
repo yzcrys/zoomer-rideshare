b22a6815168e05c00e95ba5193fe1f1aa252b701
yzcrys <deaddyui@gmail.com>
2022-11-25 02:36:07 -0500

    HOTFIX: Fix trip bugs (#23)

--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/App.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -12,6 +12,7 @@ public class App {
         // TODO: Add server contexts here. Do not set executors for the server, you shouldn't need them.
         server.createContext("/trip/request", new Request());
         server.createContext("/trip/confirm", new Confirm());
+        server.createContext("/trip/", new Trip());
 
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Trip.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Trip.java
@@ -1,7 +1,10 @@
 package ca.utoronto.utm.mcs;
 
 import com.sun.net.httpserver.HttpExchange;
+import org.bson.Document;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class Trip extends Endpoint {
@@ -17,6 +20,48 @@ public class Trip extends Endpoint {
 
     @Override
     public void handlePatch(HttpExchange r) throws IOException, JSONException {
-        // TODO
+
+        String[] split1 = r.getRequestURI().toString().split("/");
+        System.out.println("\n\n" + split1[2] + "\n\n");
+        if (split1.length != 4 || split1[3].isEmpty()) {
+            this.sendStatus(r, 400);
+            return;
+        }
+
+        String _id = split1[2];
+        String reqBody = Utils.convert(r.getRequestBody());
+        if (reqBody.equals("")) {
+            this.sendStatus(r, 400);
+            return;
+        }
+
+        JSONObject body = new JSONObject(reqBody);
+        String fields[] = {"distance", "endTime", "timeElapsed", "discount", "totalCost", "driverPayout"};
+
+        Class<?> fieldClasses[] = {Integer.class, Integer.class, Integer.class, Integer.class, Double.class, Double.class};
+        if (!validateFields(body, fields, fieldClasses)) {
+            System.out.println("in validate fields");
+            this.sendStatus(r, 400);
+            return;
+        }
+
+        int distance = body.getInt("distance");
+        int endTime = body.getInt("endTime");
+        int timeElapsed = body.getInt("timeElapsed");
+        int discount = body.getInt("discount");
+        Double totalCost = body.getDouble("totalCost");
+        Double driverPayout = body.getDouble("driverPayout");
+
+
+        int doc;
+        try {
+            System.out.println("trydao");
+            doc = dao.updateTripInfo(_id, distance, endTime, timeElapsed, discount, totalCost, driverPayout);
+            System.out.println("\n" + doc);
+            this.sendStatus(r, doc);
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.sendStatus(r, 500);
+        }
     }
 }
