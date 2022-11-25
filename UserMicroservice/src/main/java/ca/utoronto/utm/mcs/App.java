9abd3f0d8ddd9a318c0db7cc88d4e496a9e59ed7
yzcrys <deaddyui@gmail.com>
2022-11-25 00:12:00 -0500

    Develop (#18)
    * Feature/get nearby driver (#2)
    
    * Add starter code files
    
    * Create endpoint for nearbyDriver
    
    * Create trip/request database operation (#3)
    
    * Feature/API Gateway (#4)
    
    * Implement API Gateway
    
    * Fix small API Gateway bug
    
    * Feature/trip/request (#5)
    
    * Create trip/request database operation
    
    * Update trip/request call to microservice
    
    * Fix trip/request http call
    
    * Feature/trip/confirm (#6)
    
    * Create trip/confirm database operation
    
    * Update trip/confirm endpoint
    
    * Add user/register endpoint (#7)
    
    * Create trip/passenger/uid database operation method (#8)
    
    * Create trip/id database operation method (#9)
    
    * Feature/location/nearby driver (#10)
    
    * Add starter code files
    
    * Create endpoint for nearbyDriver
    
    * Feature/location/navigation (#11)
    
    * Add getNavigation endpoint
    
    * Fix small bug for getNavigation endpoint
    
    * Add location tests (#12)
    
    * Add trip endpoint tests (#14)
    
    * Hotfix revert starter (#15)
    
    * Develop (#13)
    
    * Feature/get nearby driver (#2)
    
    * Add starter code files
    
    * Create endpoint for nearbyDriver
    
    * Create trip/request database operation (#3)
    
    * Feature/API Gateway (#4)
    
    * Implement API Gateway
    
    * Fix small API Gateway bug
    
    * Feature/trip/request (#5)
    
    * Create trip/request database operation
    
    * Update trip/request call to microservice
    
    * Fix trip/request http call
    
    * Feature/trip/confirm (#6)
    
    * Create trip/confirm database operation
    
    * Update trip/confirm endpoint
    
    * Add user/register endpoint (#7)
    
    * Create trip/passenger/uid database operation method (#8)
    
    * Create trip/id database operation method (#9)
    
    * Feature/location/nearby driver (#10)
    
    * Add starter code files
    
    * Create endpoint for nearbyDriver
    
    * Feature/location/navigation (#11)
    
    * Add getNavigation endpoint
    
    * Fix small bug for getNavigation endpoint
    
    * Add location tests (#12)
    
    * HOTFIX: revert starter code
    
    * HOTFIX: small typos (#17)

--- UserMicroservice/src/main/java/ca/utoronto/utm/mcs/App.java
+++ UserMicroservice/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -14,6 +14,7 @@ public class App {
 
         // TODO: Add server contexts here. Do not set executors for the server, you shouldn't need them.
         server.createContext("/user/register", new Register());
+        server.createContext("/user/login", new Login());
 
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
